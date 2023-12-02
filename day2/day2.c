85711e373a838ad5d48698de4e55e0945309712a
MrRys <Richard.letanec@gmail.com>
2023-12-02 10:16:50 +0100

    Day 2 C Solution

---
+++ day2/day2.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define LIMIT_RED 12
+#define LIMIT_GREEN 13
+#define LIMIT_BLUE 14
+
+#define MAX(a, b) (a > b ? a : b)
+
+bool check_possible(char color, int count)
+{
+    switch (color)
+    {
+    case 'r':
+        return count <= LIMIT_RED;
+    case 'g':
+        return count <= LIMIT_GREEN;
+    case 'b':
+        return count <= LIMIT_BLUE;
+    default:
+        return false;
+    }
+}
+
+int solve()
+{
+    int result = 0;
+
+    int game;
+    while (scanf("Game %d:", &game) > 0)
+    {
+        int count;
+        char color;
+        bool ispossible = true;
+        while (scanf("%*c%d %c%*[^,;\n]", &count, &color) > 0)
+        {
+            if (!check_possible(color, count))
+            {
+                ispossible = false;
+            }
+        }
+        if (ispossible)
+        {
+            result += game;
+        }
+    }
+    return result;
+}
+
+int solve_hard()
+{
+    int result = 0;
+
+    int game;
+    while (scanf("Game %d:", &game) > 0)
+    {
+        int count;
+        char color;
+        int r = 0, g = 0, b = 0;
+        while (scanf("%*c%d %c%*[^,;\n]", &count, &color) > 0)
+        {
+            switch (color)
+            {
+            case 'r':
+                r = MAX(r, count);
+                break;
+            case 'g':
+                g = MAX(g, count);
+                break;
+            case 'b':
+                b = MAX(b, count);
+                break;
+            default:
+                break;
+            }
+        }
+        result += r * g * b;
+        r = g = b = 0;
+    }
+    return result;
+}
+
+int main(int argc, char *argv[])
+{
+    bool ishard = argc > 1 && argv[1][0] == 'h';
+
+    int result = ishard ? solve_hard() : solve();
+    printf("%d\n", result);
+}
