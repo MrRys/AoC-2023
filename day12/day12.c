1dc1448625d7b955f7cbdf477acf4e45240b1eaf
MrRys <Richard.letanec@gmail.com>
2023-12-19 21:01:33 +0100

    formatting

--- day12/day12.c
+++ day12/day12.c
@@ -65,8 +65,9 @@ unsigned long hash_str(char *str) {
     unsigned long hash = 5381;
     int c;
 
-    while ((c = *str++))
+    while ((c = *str++)) {
         hash = ((hash << 5) + hash) + c;
+    }
 
     return hash;
 }
